9430a85f0653a175e275c6178ddfa592bb1ad638
Mahmoud Shaqfa <mahmoudshaqfa@EESDDebian01.intranet.epfl.ch>
2021-06-01 10:55:35 +0200

    License added.

--- code/HSH_basis_range.m
+++ code/HSH_basis_range.m
@@ -1,3 +1,45 @@
+%*************************************************************************%
+% All rights reserved (C) to the authors: Mahmoud SHAQFA, Gary CHOI       %
+%		 and Katrin BEYER                                                 %
+%                                                                         %
+% M. Shaqfa Contact:                                                      %
+% Earthquake Engineering and Structural Dynamics Laboratory (EESD),       %
+% School of Architecture, Civil and Environmental Engineering (ENAC),     %
+% Ecole polytechnique federale de Lausanne (EPFL),                        %
+% CH-1015 Lausanne, Switzerland.                                          %
+%               Tel.: +41 21 69 33297                                     %
+%               Email: mahmoud.shaqfa@epfl.ch                             %
+%                                                                         %
+% G. Choi Contact:                                                        %
+% Department of Mathematics, Massachusetts Institute of Technology (MIT)  %
+% Cambridge, MA, USA                                                      %
+%               Email: ptchoi@mit.edu                                     %
+%                                                                         %
+% K. Beyer Contact:                                                       %
+%               Email: katrin.beyer@epfl.ch                               %
+%*************************************************************************%
+% This code includes implementations for:                                 %
+%				- Spherical Cap Harmonics (SCH)                           %
+%				- Spherical Harmonics (SH)                                %
+%				- HemiSpherical Harmonics (HSH)                           %
+% This code is part of the paper: "Spherical Cap Harmonic Analysis(SCHA)..%
+%	 for Characterising the Morphology of Rough Surface Patches"          %
+%                                                                         %
+%*************************************************************************%
+% This library is free software; you can redistribute it and/or modify	  %
+% it under the terms of the GNU Lesser General Public License as published%
+% by the Free Software Foundation; either version 2.1 of the License, or  %
+% (at your option) any later version.                                     %
+%                                                                         %
+% This library is distributed in the hope that it will be useful,         %
+% but WITHOUT ANY WARRANTY; without even the implied warranty of          %
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                    %
+% See the GNU Lesser General Public License for more details.        	  %
+% You should have received a copy of the GNU Lesser General Public License%
+% along with this library; if not, write to the Free Software Foundation, %
+% Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA       %
+%*************************************************************************%
+% Author of this file: Mahmoud S. Shaqfa
 function reconstruction = HSH_basis_range(phi, theta, k_range, qm_k)
 k_min = min(k_range);
 k_max = max(k_range);
